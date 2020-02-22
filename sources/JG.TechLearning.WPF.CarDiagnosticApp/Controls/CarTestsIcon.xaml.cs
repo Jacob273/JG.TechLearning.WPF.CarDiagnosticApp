5b3cf13736f1ac5ddf578fbc9116e07116c80621
Jakub Gmur <jakubgmur@Op.pl>
2020-02-23 00:56:55 +0100

    Added CarTestsIcon Changed Progress Window a bit

---
+++ sources/JG.TechLearning.WPF.CarDiagnosticApp/Controls/CarTestsIcon.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JG.TechLearning.WPF.CarDiagnosticApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for CarTestsIcon.xaml
+    /// </summary>
+    public partial class CarTestsIcon : UserControl
+    {
+        public CarTestsIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
