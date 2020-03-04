af97526b95c900aaad04e61a8c589c44f960da9b
Jakub Gmur <jakubgmur@Op.pl>
2020-03-04 22:57:43 +0100

    Fixed build failing after GlobalCommonTypes csproj removal

--- sources/JG.TechLearning.WPF.CarDiagnostic.ViewModel/LoggingViewModel.cs
+++ sources/JG.TechLearning.WPF.CarDiagnostic.ViewModel/LoggingViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using JG.TechLearning.WPF.CarDiagnostic.Common;
-using JG.TechLearning.WPF.CarDiagnostic.GlobalCommonTypes;
 using System.Collections.ObjectModel;
 
 namespace JG.TechLearning.WPF.CarDiagnostic.ViewModel
